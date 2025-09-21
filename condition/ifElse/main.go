351e84de8eba0b1afeb5bc85083df514d972c9f9
Chintukr <chintukr1904@gmail.com>
2025-09-21 22:47:07 +0530

    if else

--- condition/ifElse/main.go
+++ condition/ifElse/main.go
@@ -11,13 +11,13 @@ func main() {
 	if err!=nil{
 		fmt.Println("error", err)
 	
-	}
+	} 
 
 	if (num == 0 || num > 0) && (num % 2 == 0 ) { 
 		fmt.Println("value is positive and also divisible")
 	}else if (num == 0 || num > 0) && (num % 2 != 0 ){
 		fmt.Println("value is positive but not div by 2")
-		
+		 
 	} else if num < 0 {
 		
 		fmt.Println("value is neg")
