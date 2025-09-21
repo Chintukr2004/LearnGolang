22b96cd7da144da120fe97e0c48e19ff3ad38d60
Chintukr <chintukr1904@gmail.com>
2025-09-21 16:40:53 +0530

    using poinnter

--- dataType/main.go
+++ dataType/main.go
@@ -8,13 +8,14 @@ type Student struct {
 	name string
 	age  int
 	gpa  float32
+	addr Addr
 }
 
-type Employee struct{
-	name string
-	age int
-	salary float32
+type Addr struct{
+	vill string
+	city string
 }
+
 
 type Phone struct{
 	BasicInfo BasicInfo
@@ -46,12 +47,15 @@ func main() {
 	s1.name = "John"
 	s1.age = 21
 	s1.gpa = 3.5
+	s1.addr.city = "hazaribagh"
+	s1.addr.vill = "padma"
 	fmt.Println(s1)
+	
+	s2 :=Student{name: "shivam", age: 22, gpa: 6.6, addr: Addr{city: "ranchi", vill: "kuma"}}
+	
+	fmt.Println(s2)
 
-	eam := Employee{"Arv", 22, 50000.50}
-
-	fmt.Println(eam)
-
+	
 	val := 122
 	val2 := "1231"
 
@@ -61,6 +65,15 @@ func main() {
 	fmt.Println("Interface value:",interfaceExample)
 	interfaceExample = val2
 	fmt.Println("Interface value:",interfaceExample)
+
+
+
+	var vishladress *Addr
+	vishladress = &s1.addr
+
+	shivamAddress := &s2.addr
+	
+	fmt.Println("vishal Adress: ", vishladress , "shivamAddress" , shivamAddress)
 
 }
 
